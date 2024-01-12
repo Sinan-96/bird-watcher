14afa0c881e66db14649e3ac9b364f8609dce162
Sinan Selboe Karagülle <sinan.karagulle@sparebank1.no>
2024-01-12 19:25:05 +0100

    Add google picture API

---
+++ bird-watcher/src/routes/api/images/[bird]/+server.ts
@@ -0,0 +1,7 @@
+import { env } from "$env/dynamic/private";
+import { json } from "@sveltejs/kit";
+
+export async function GET({params}) {
+    const images = await fetch(`https://www.googleapis.com/customsearch/v1?key=${env.GOOGLE_API_KEY}&cx=${env.GOOGLE_SEARCH_MOTOR_ID}&searchType=image&q=${params.bird}&num=10`)
+    return json({images: (await images.json()).items});
+  }
--- bird-watcher/src/routes/birds/+page.svelte
+++ bird-watcher/src/routes/birds/+page.svelte
@@ -4,11 +4,29 @@ import { TextInput, Button } from '@svelteuidev/core';
     import UploadWidget from '../../components/upload-widget.svelte';
 
 
-let birdName = '';
+export let birdName = '';
+let images = [];
+
+function getImages(){
+    fetch(`api/images/${birdName}`, {method:'GET'})
+    .then(response => response.json())
+    .then(data => images = data.images)
+    .catch(error => console.log(error))
+}
+
 </script>
+
 <Header/>
 <form method="post" action="/birds/create">
     <TextInput type="text" label="name" placeholder="Navn på fugl" bind:value={birdName} />
     <UploadWidget/>
     <Button type="submit">Last opp fugl</Button>
+
+
 </form>
+<Button on:click={getImages}> søk </Button>
+{#each images as image}
+<img src={image.link} alt="image"/>
+{/each}
+
+
