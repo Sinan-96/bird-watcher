5b226fbff185a4402a3bc8bdb654b359c1d5d7a8
Sinan96 <sinan2009@gmail.com>
2024-02-18 11:00:46 +0100

    Navbar for mobile

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let open = false
+</script>
+
+<aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:open>
+	<nav class="p-12 text-xl">
+        <li><a class="text-2xl font" href="/">Forside</a></li>
+        <li><a class="text-2xl" href="/stats">Statistikk</a></li>
+        <li><a class="text-2xl" href="/contact">Kontakt</a></li>
+        <li><a class="text-2xl" href="/birds/create">Fugler</a></li>
+	</nav>
+</aside>
+
+<style>
+	aside {
+		left: -100%;
+		transition: left 0.3s ease-in-out
+	}
+	
+	.open {
+		left: 0
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,19 @@
     import "../app.css";
     import { SvelteUIProvider } from '@svelteuidev/core';
     import Header from "../components/header.svelte";
+	import Navbar from '../components/Navbar.svelte';
+  import Sidebar from '../components/Sidebar.svelte';
+  import isMobile from "is-mobile";
+
+  let open = false
   </script>
-<Header/>
+
+{#if isMobile() }
+<Sidebar bind:open/>
+<Navbar bind:sidebar={open}/>
+  {:else}
+  <Header/>
+{/if}
   <SvelteUIProvider>
     <slot />
 </SvelteUIProvider>
