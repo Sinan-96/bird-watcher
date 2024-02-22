6421e9a313c9be28471987047e1838abb3148882
Sinan96 <sinan2009@gmail.com>
2024-02-22 16:55:53 +0100

    Add new entrance for creating new bird

--- src/components/bird-type/bird-type-list.svelte
+++ src/components/bird-type/bird-type-list.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import type { Bird } from "@prisma/client";
     import BirdType from "./bird-type.svelte";
-	import { TextInput } from "@svelteuidev/core";
+	import { TextInput, ActionIcon } from "@svelteuidev/core";
+	import { Plus } from "radix-icons-svelte";
 
     export let birds: Bird[]
     export let birdSighteings: any[]
@@ -18,9 +19,6 @@
     }
 </script>
 
-{#if filteredBirds.length === 0}
-    <h2 class="text-lg">Ingen fugler funnet</h2>
-{/if}
 
 <div class="flex justify-center mt-10">
     <TextInput type="text" name="name" placeholder="Søk på fugl" bind:value={birdName} class="m-auto" />
@@ -31,5 +29,16 @@
         <BirdType removeBird={removeBird} bird={bird} count={getCount(bird.id)}/>
     {/each}
     </div>
+    {#if filteredBirds.length === 0}
+    <div class="flex flex-col items-center gap-4">
+        <h2 class="text-lg mt-6">Ingen fugler funnet</h2>
+        <a href="/birds/create">
+            <ActionIcon  size="2xl">
+                <Plus size={50} />
+            </ActionIcon>
+        </a>
+    </div>
+{/if}
+
 </div>
 
