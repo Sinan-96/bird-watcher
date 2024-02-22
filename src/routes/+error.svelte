8c5c1ed3ce1e74442cf8bb4dbfa6a4ef6791d4a4
Sinan96 <sinan2009@gmail.com>
2024-02-22 17:12:49 +0100

    La til en default feil side

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col mx-4 mt-4 mb-4">
+    <p class="text-2xl font-mono">
+        Ops! Her gikk noe galt. Enten har du skrevet inn en feil URL, eller så har vi gjort en feil. Uansett, vi beklager.
+        Ta kontakt med en av våre helter i bildet under eller med deres kommandør Sinan. Håper dette ordner seg!
+    </p>
+    <img src="/images/helter.jpg" alt="Helter" class="mt-4" />
+</div>
