092eb9d5e1d457ab01eb105c38bb82e136e5c51e
Sinan96 <sinan2009@gmail.com>
2024-02-22 10:10:14 +0100

    Fix header and title for mobile

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -4,20 +4,22 @@
 
 <aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:open>
 	<nav class="p-12 text-xl">
-        <li><a class="text-2xl font" href="/">Forside</a></li>
-        <li><a class="text-2xl" href="/stats">Statistikk</a></li>
-        <li><a class="text-2xl" href="/contact">Kontakt</a></li>
-        <li><a class="text-2xl" href="/birds/create">Fugler</a></li>
+        <li><a class="text-2xl font" href="/" on:click={() => open = false}>Forside</a></li>
+        <li><a class="text-2xl" href="/stats" on:click={() => open = false}>Statistikk</a></li>
+        <li><a class="text-2xl" href="/contact" on:click={() => open = false}>Kontakt</a></li>
+<li><a class="text-2xl" href="/birds/create" on:click={() => open = false}>Fugler</a></li>
 	</nav>
 </aside>
+
 
 <style>
 	aside {
 		left: -100%;
-		transition: left 0.3s ease-in-out
+		transition: left 0.3s ease-in-out;
+		z-index: 9999; /* Set a high z-index value to make the component the topmost layer */
 	}
 	
 	.open {
-		left: 0
+		left: 0;
 	}
 </style>
--- src/components/title.svelte
+++ src/components/title.svelte
@@ -1,6 +1,6 @@
 
 <div class="flex justify-center mb-4">
-    <img class="h-16 rounded w-auto mr-4" src="images/logo.png" sizes="" alt="">
-<h1 class="text-5xl sm:text-2xl font-extrabold self-center">Fugletitterne</h1>
+    <img class="md:h-16 h-10 rounded w-auto mr-4" src="images/logo.png" sizes="" alt="">
+<h1 class="md:text-5xl text-3xl font-extrabold self-center">Fugletitterne</h1>
 
 </div>
