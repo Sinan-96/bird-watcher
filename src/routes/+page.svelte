16abe6e6189b629f238d9d9b2afb306d36d923c9
Sinan96 <sinan2009@gmail.com>
2024-02-22 10:33:39 +0100

    More fixes to mobile header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,14 @@
     import BirdTypeList from "../components/bird-type/bird-type-list.svelte";
 	import LocationSelector from "../components/location-selector.svelte";
     import { SvelteUIProvider } from '@svelteuidev/core';
+	import isMobile from "is-mobile";
 
     export let data;
 </script>
 <SvelteUIProvider class="font-mono">
+    {#if isMobile() }
     <Title/>
+    {/if}
     <LocationSelector locations={data.locations.concat({id: 0, name:"Alle steder"})} />
     <BirdTypeList birds = {data.birds} birdSighteings = {data.birdSighteings}/>
 </SvelteUIProvider>
